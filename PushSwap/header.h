3e97538a020e8adf8ac110afafef277c518e6358
Mohammed Maarafi <mmaarafi@c2r4p7.1337.ma>
2025-02-06 21:35:21 +0100

    alo

--- PushSwap/header.h
+++ PushSwap/header.h
@@ -6,7 +6,7 @@
 /*   By: mmaarafi <mmaarafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 17:37:28 by mmaarafi          #+#    #+#             */
-/*   Updated: 2025/02/06 20:34:56 by mmaarafi         ###   ########.fr       */
+/*   Updated: 2025/02/06 21:35:07 by mmaarafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,5 @@ void			swap(t_stack **stack_a);
 void			swap_both(t_stack **stack_a, t_stack **stack_b);
 void			push(t_stack **stack1, t_stack **stack2);
 void			rotate(t_stack **stack);
+void			ft_lstadd_front(t_stack **lst, t_stack *new);
 #endif
---
+++ PushSwap/rrotate.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+void	rotate(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack *ptr;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+		return ;
+	ptr = *stack;
+	while(ptr)
+	{
+		if((ptr->next)->next == NULL)
+		{
+			tmp = ptr->next;
+			ptr->next = NULL;
+			break;
+		}
+	}
+	ft_lstadd_front(stack, tmp);
+}
