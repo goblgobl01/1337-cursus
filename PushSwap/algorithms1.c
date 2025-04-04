7344bd093c5d115941e106be8cb71faeafcbcbb9
goblgobl01 <133246212+goblgobl01@users.noreply.github.com>
2025-04-04 18:01:47 +0000

    alo

--- PushSwap/algorithms1.c
+++ PushSwap/algorithms1.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:48:16 by mmaarafi          #+#    #+#             */
-/*   Updated: 2025/02/25 17:33:47 by codespace        ###   ########.fr       */
+/*   Updated: 2025/04/04 18:01:21 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,29 @@ int	positioning(t_stack *tmp, int i)
 	return (pos);
 }
 
+void	handle_position(int pos, int i, t_stack **stack_a, t_stack **stack_b)
+{
+	if (pos == 1)
+		sa(stack_a);
+	else if (pos == 2)
+	{
+		ra(stack_a);
+		sa(stack_a);
+	}
+	else if (pos == 3)
+	{
+		if (i == 0)
+			rra(stack_a);
+		rra(stack_a);
+	}
+	else if (pos == 4)
+	{
+		if (i == 0)
+			rra(stack_a);
+	}
+	pb(stack_a, stack_b);
+}
+
 void	smallest_element(t_stack **stack_a, t_stack **stack_b)
 {
 	int	pos;
@@ -58,34 +81,7 @@ void	smallest_element(t_stack **stack_a, t_stack **stack_b)
 			pos = positioning(*stack_a, 0);
 		else
 			pos = positioning(*stack_a, i);
-		if (pos == 0)
-			pb(stack_a, stack_b);
-		else if (pos == 1)
-		{
-			sa(stack_a);
-			pb(stack_a, stack_b);
-		}
-		else if (pos == 2)
-		{
-			ra(stack_a);
-			sa(stack_a);
-			pb(stack_a, stack_b);
-		}
-		else if (pos == 3)
-		{
-			if (i == 0)
-				rra(stack_a);
-			rra(stack_a);
-			pb(stack_a, stack_b);
-		}
-		else if (pos == 4)
-		{
-			if (i == 0)
-			{
-				rra(stack_a);
-				pb(stack_a, stack_b);
-			}
-		}
+		handle_position(pos, i, stack_a, stack_b);
 		i++;
 	}
 }
